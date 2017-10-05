25f0b04f6694a4b1b43f05472ff6eb541b045532
Arne Decant <hello@arnedecant.be>
2017-10-05 23:42:45 +0200

    implementation YQL

--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,9 @@ function init() {
 		let $form = $(this);
 		let address = $form.find('input.address').val();
 
-		$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(address) + '&callback=?', function(data){
+		// $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(address) + '&callback=?', function(data){
+		requestCrossDomain(address, function(data) {
+			console.log(data);
 			let results = getResults(data.contents, 'a');
 			let html = '<ul>';
 
@@ -89,4 +91,42 @@ String.prototype.getBetween = function(start, end) {
 	let result = this.split(start)[1];
 	if (!result) return;
 	return result.split(end)[0];
+}
+
+// Accepts a url and a callback function to run.
+function requestCrossDomain( site, callback ) {
+     
+    // If no url was passed, exit.
+    if ( !site ) {
+        console.error('No site was passed.');
+        return false;
+    }
+     
+    // Take the provided url, and add it to a YQL query. Make sure you encode it!
+    var yql = "select * from htmlstring where url='" + site + "' AND xpath='//div'";
+   	var resturl = "http://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent(yql) + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+   	console.log(resturl);
+    
+    // Request that YSQL string, and run a callback function.
+    // Pass a defined function to prevent cache-busting.
+    $.getJSON( resturl, cbFunc );
+     
+    function cbFunc(data) {
+    	// console.log(data);
+	    // If we have something to work with...
+	    if ( data.results[0] ) {
+	        // Strip out all script tags, for security reasons.
+	        // BE VERY CAREFUL. This helps, but we should do more. 
+	        data = data.results[0].replace(/<script[^>]*>[\s\S]*?<\/script>/gi, '');
+	         
+	        // If the user passed a callback, and it
+	        // is a function, call it, and send through the data var.
+	        if ( typeof callback === 'function') {
+	            callback(data);
+	        }
+	    }
+	    // Else, Maybe we requested a site that doesn't exist, and nothing returned.
+	    else throw new Error('Nothing returned from getJSON.');
+    }
 }
