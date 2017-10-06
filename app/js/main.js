f7c62e33f06ca2772fe948b3360091be15618f88
Arne Decant <hello@arnedecant.be>
2017-10-06 21:22:03 +0200

    fetch images

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,12 @@
-const PROPERTIES = {
+const ELEMENTS = {
 	anchor: {
 		name: 'anchor',
-		element: 'a',
+		tag: 'a',
 		scrape: 'href'
 	},
 	image: {
 		name: 'image',
-		element: 'img',
+		tag: 'img',
 		scrape: 'src'
 	}
 }
@@ -16,13 +16,11 @@ jQuery(document).ready(function($){
 });
 
 function init() {
-	for (let key in PROPERTIES) {
-		if (!PROPERTIES.hasOwnProperty(key)) continue;
-		let prop = PROPERTIES[key];
+	for (let key in ELEMENTS) {
+		if (!ELEMENTS.hasOwnProperty(key)) continue;
+		let el = ELEMENTS[key];
 
-		console.log(prop.element);
-
-		let option = '<option value="'+prop.name+'" data-name="'+prop.name+'" data-element="'+prop.element+'" data-scrape="'+prop.scrape+'">'+prop.name+'</option>'
+		let option = '<option value="'+el.name+'" data-name="'+el.name+'" data-tag="'+el.tag+'" data-scrape="'+el.scrape+'">'+el.name+'</option>'
 		$('form.scraper select.element').append(option);
 	}
 
@@ -32,20 +30,32 @@ function init() {
 
 		let $form = $(this);
 		let address = $form.find('input.address').val();
+		let element = ELEMENTS[$form.find('select.element').val()];
+
+		if (!element) alert('An error has occurred.');
 
 		$.getJSON('https://whateverorigin.herokuapp.com/get?url=' + encodeURIComponent(address) + '&callback=?', function(data){
-		// $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(address) + '&callback=?', function(data){
-			let results = getResults(data.contents, 'a');
+			let results = getResults(data.contents, element);
+
+			console.log(results);
+
 			let html = '<ul>';
 
 			if (results) {
 				for (let i = 0, r; r = results[i]; i++) {
 					html += '<li>';
-					html += '<a href="' + r + '" target="_blank">' + r + '</a>';
 
-					if (isInternalUrl(address, r)) {
-						console.log(r);
+					if (element.tag === 'a') {
+						html += '<a href="' + r + '" target="_blank">' + r + '</a>';
+
+						if (isInternalUrl(address, r)) {
+							console.log(r);
+						}
+					} else if (element.tag === 'img') {
+						html += '<img src="" />'
 					}
+
+					
 
 					html += '</li>';
 				}
@@ -59,16 +69,17 @@ function init() {
 
 function getResults(content, element) {
 	let results = [];
-	let data = content.split('<' + element + ' ');
+	let data = content.split('<' + element.tag + ' ');
+	let scrape = element.scrape + '=';
 
 	for (let i = 1, part; part = data[i]; i++) {
-		let url = part.getBetween("href='", "'");
+		let url = part.getBetween(scrape + "'", "'");
 
 		if (!url) {
-			url = part.getBetween('href="', '"');
+			url = part.getBetween(scrape + '"', '"');
 		}
 
-		if (url.startsWith("http")) {
+		if ((element.tag === 'a' && url.startsWith("http")) || element.tag === 'img') {
 			results.push(url);
 		}
 	}
@@ -113,42 +124,4 @@ String.prototype.getBetween = function(start, end) {
 	let result = this.split(start)[1];
 	if (!result) return;
 	return result.split(end)[0];
-}
-
-// Accepts a url and a callback function to run.
-function requestCrossDomain( site, callback ) {
-     
-    // If no url was passed, exit.
-    if ( !site ) {
-        console.error('No site was passed.');
-        return false;
-    }
-     
-    // Take the provided url, and add it to a YQL query. Make sure you encode it!
-    var yql = "select * from htmlstring where url='" + site + "' AND xpath='//div'";
-   	var resturl = "http://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent(yql) + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-
-   	console.log(resturl);
-    
-    // Request that YSQL string, and run a callback function.
-    // Pass a defined function to prevent cache-busting.
-    $.getJSON( resturl, cbFunc );
-     
-    function cbFunc(data) {
-    	// console.log(data);
-	    // If we have something to work with...
-	    if ( data.results[0] ) {
-	        // Strip out all script tags, for security reasons.
-	        // BE VERY CAREFUL. This helps, but we should do more. 
-	        data = data.results[0].replace(/<script[^>]*>[\s\S]*?<\/script>/gi, '');
-	         
-	        // If the user passed a callback, and it
-	        // is a function, call it, and send through the data var.
-	        if ( typeof callback === 'function') {
-	            callback(data);
-	        }
-	    }
-	    // Else, Maybe we requested a site that doesn't exist, and nothing returned.
-	    else throw new Error('Nothing returned from getJSON.');
-    }
 }
